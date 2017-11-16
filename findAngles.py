cb1fe8a9883a837d0a09280f31423b4ccef44f23
Henry Rachootin <henry.rachootin@students.olin.edu>
2017-11-15 22:36:15 -0500

    Made the normal vector finding code. It takes 3 ms to find the vector and the angles

--- findAngles.py
+++ findAngles.py
@@ -1,6 +1,8 @@
 from scipy.optimize import brenth
 from math import *
 
+f_tol = 1e-14
+
 def getLen0(x0,x,y,ux,uy,uz):
   return sqrt((-3.13934208871859 + (3.405049132851517*uz)/sqrt(ux**2 + uz**2) + (0.6141989928900345*ux*uy)/(sqrt(ux**2 + uz**2)*sqrt(ux**2 + uy**2 + uz**2)) + x + 0.5000000000000001*cos(x0))**2 + (1.8125 + (-0.6141989928900345*ux**2 - 0.6141989928900345*uz**2)/(sqrt(ux**2 + uz**2)*sqrt(ux**2 + uy**2 + uz**2)) + y + 0.8660254037844387*cos(x0))**2 + (-12.43 + (3.405049132851517*ux)/sqrt(ux**2 + uz**2) - (0.6141989928900345*uy*uz)/(sqrt(ux**2 + uz**2)*sqrt(ux**2 + uy**2 + uz**2)) + sin(x0))**2) - 12.625
 
@@ -24,5 +26,5 @@ funcs = [getLen0, getLen1, getLen2, getLen3, getLen4, getLen5]
 def findAngles(x,y,ux,uy,uz):
   thetas = [0,0,0,0,0,0]
   for i in range(6):
-    thetas[i] = brenth(funcs[i], radians(-70), radians(70), disp = True, args =(x,y,ux,uy,uz))
+    thetas[i] = brenth(funcs[i], radians(-70), radians(70), disp = True, args =(x,y,ux,uy,uz), xtol = 1e-3)
   return thetas
--- findNormal.py
+++ findNormal.py
@@ -1,6 +1,8 @@
 from math import *
 import numpy as np
 from scipy.optimize import broyden1
+from findAngles import findAngles
+from time import time
 
 p = 0.7
 s = 75
@@ -9,23 +11,51 @@ def getFinalT(vx, vy, vz, phi, theta):
   costh = cos(theta)
   sinth = sin(theta)
   return (2*s + 2*p*s - 2*p*vz + phi**2*vz + p*phi**2*vz - 2*phi*vx*costh - 2*p*phi*vx*costh - 2*phi*vy*sinth - 2*p*phi*vy*sinth)/g
-  
+
 def getX(vx, vy, vz, phi, theta, t, rx):
   costh = cos(theta)
   sinth = sin(theta)
   return rx + (t*(vx - phi*vz*costh - phi**2*costh*sinth*(vy - phi*vz*sinth + phi**2*costh*(vy*costh - vx*sinth)) + phi*costh*(1 + phi**2*costh**2)*(s + p*(s - vz - phi*(vx*costh + vy*sinth)))))/(1 + phi**2*costh**2)
-    
+
 def getY(vx, vy, vz, phi, theta, t, ry):
   costh = cos(theta)
   sinth = sin(theta)
   return ry + t*(vy - (1 + p)*phi*vz*sinth + phi*(phi*vy*costh**2 + (1 + p)*(s - phi*vx*costh)*sinth - p*phi*vy*sinth**2))
-  
+
 def getFinalPos(x, vx, vy, vz, rx, ry):
   phi   = x[0]
   theta = x[1]
   t = getFinalT(vx, vy, vz, phi, theta)
   return np.array([getX(vx,vy,vz,phi,theta,t,rx), getY(vx,vy,vz,phi,theta,t,ry)])
-  
-def findAngles(vx, vy, vz, rx, ry):
-  return broyden1(lambda x:getFinalPos(x,vx,vy,vz,rx,ry), [0.1,-atan2(ry,rx)])
-  
+
+def findTilt(vx, vy, vz, rx, ry):
+  return broyden1(lambda x:getFinalPos(x,vx,vy,vz,rx,ry), [0.2,-atan2(ry,rx)], f_tol = 1e-14)
+
+def findNormal(vx, vy, vz, rx, ry):
+  x = findTilt(vx,vy,vz,rx,ry)
+  phi   = x[0]
+  theta = x[1]
+
+  if(theta > 0):
+    theta = theta%(2*pi)
+
+  print("phi =",degrees(phi))
+  print("theta=",degrees(theta))
+  ux, uy, uz = cos(theta)*sin(phi), sin(theta)*sin(phi), cos(phi)
+  return [ux, uy, uz]
+
+
+if(__name__ == '__main__'):
+  x0 = 1
+  y0 = 1
+  v0 = -10
+  vx0 = 2;
+  vy0 = 1;
+  tBefore = time()
+  u = findNormal(vx0,vy0,v0,x0,y0)
+  print("pos =",x0,y0)
+  print("u =",u)
+  angles = findAngles(x0,y0,u[0],u[1],u[2])
+  tTotal = time() - tBefore
+  #print(angles)
+  #print(tTotal*1000)
